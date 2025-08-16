4f99fb94d019413d13c0d76dea69afaee707ddcc
flukester <flukester1@outlook.com>
2025-08-16 14:42:33 +0500

    asdasda

--- src/pages/TgCallback.jsx
+++ src/pages/TgCallback.jsx
@@ -31,7 +31,7 @@ const TelegramCallback = () => {
     }
 
     // 🚀 Send to backend for verification
-    fetch("http://localhost:8000/api/v1/auth/telegram", {
+    fetch("http://localhost:3000/api/v1/auth/telegram", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(authData),
